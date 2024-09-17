91af0d93d2758489e65f191483c6272e586af5a3
abhi <abhi@gmail.com>
2024-09-17 22:05:26 +0530

    program to add two number

---
+++ ab.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the value of a and b";
+    int a,b;
+    cin >> a >> b;
+    int sum=a+b;
+    cout<< "sum ="<<sum << endl;
+    return 0;
+}
